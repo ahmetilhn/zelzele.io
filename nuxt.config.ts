e8bde1e5f7eb0009c4831c8ebf910b57d90a6a57
ahmetilhan24 <ahmetilhan.dev@gmail.com>
2023-02-24 18:13:24 +0300

    ⚪️ style resources added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    vite: {
+        css: {
+          preprocessorOptions: {
+            scss: {
+              additionalData: '@import "@/assets/scss/variables.scss";',
+            },
+          },
+        },
+    },
+    css: ["@/assets/scss/global.scss", "@/assets/scss/fonts.scss"]
 })
