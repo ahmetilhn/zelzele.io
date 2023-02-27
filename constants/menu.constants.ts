2382a7fc6a7e5f3a0bda2f0e7415dbaf3d8f2265
Ahmet ilhan <ahmetilhan.dev@gmail.com>
2023-02-27 18:53:32 +0300

    bottom bar created

---
+++ constants/menu.constants.ts
@@ -0,0 +1,14 @@
+export default {
+  MAP: {
+    label: "Harita",
+    val: "map",
+  },
+  LISTING: {
+    label: "Liste",
+    val: "listing",
+  },
+  INFO: {
+    label: "Bilgi",
+    val: "info",
+  },
+};
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,13 @@
     <div class="layout__content">
       <slot />
     </div>
+    <BottomBar />
   </main>
 </template>
 
 <script setup lang="ts">
 import AppHeader from "~~/components/partials/app-header.vue";
+import BottomBar from "~~/components/partials/bottom-bar.vue";
 </script>
 <style lang="scss" scoped>
 .layout {
