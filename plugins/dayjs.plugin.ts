7e791455a3d496f68fbf6e54ae29646c21afbb0c
Ahmet ilhan <ahmetilhan.dev@gmail.com>
2023-02-28 21:12:49 +0300

    date bug fixed

--- plugins/dayjs.plugin.ts
+++ plugins/dayjs.plugin.ts
@@ -1,7 +1,9 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import UTC from "dayjs/plugin/utc";
 import("dayjs/locale/tr");
 export default defineNuxtPlugin(() => {
+  dayjs.extend(UTC);
   dayjs.locale("tr");
   dayjs.extend(relativeTime);
   return {
