c77f3892db44ede4da32f50d91808c0e27cfadd3
Ahmet İlhan <60369989+ahmetilhn@users.noreply.github.com>
2023-03-05 20:16:02 +0300

    Refactor (#29)
    * 📦 afad scraper pack added
    
    * 🧼 content improved

--- server/api/earthquakes.ts
+++ server/api/earthquakes.ts
@@ -1,52 +1,13 @@
-import cheerio from "cheerio";
+import AfadScraper from "afad-scraper";
 import EarthquakeInterface from "~~/interfaces/earthquake.interface";
 export default defineEventHandler(async (event) => {
   if (event.node.req.method === "GET") {
-    const afadWeb = await $fetch<Promise<string>>(
-      "https://deprem.afad.gov.tr/last-earthquakes.html"
-    );
-    const $ = cheerio.load(afadWeb);
-    let data: Array<EarthquakeInterface> = [];
-    const parse = (): void => {
-      $("tbody")
-        .find("tr")
-        .map((i, el) => {
-          const [
-            CreatedDate,
-            Latitude,
-            Longitude,
-            Depth,
-            Type,
-            Magnitude,
-            Region,
-            ID,
-          ] = $(el).find("td");
-          data.push({
-            ID: $(ID).text(),
-            Date: $(CreatedDate).text().replaceAll("-", "/"),
-            Latitude: Number($(Latitude).text()),
-            Longitude: Number($(Longitude).text()),
-            Depth: Number($(Depth).text()),
-            Magnitude: Number($(Magnitude).text()),
-            Region: parseRegion($(Region).text()),
-            Type: $(Type).text(),
-          });
-        });
-    };
-    parse();
-    return data;
+    const scraper = new AfadScraper();
+    try {
+      const res: Array<EarthquakeInterface> = await scraper.getEarthquakes();
+      return res;
+    } catch (error) {
+      console.error(error);
+    }
   }
 });
-
-const parseRegion = (region: string): EarthquakeInterface["Region"] => {
-  let [district, city] = region?.split("("); // -> istanbul)
-  city = city?.split(")")[0]; // -> istanbul
-  if (!city) {
-    city = district;
-    district = "-";
-  }
-  return {
-    City: city,
-    District: district,
-  };
-};
