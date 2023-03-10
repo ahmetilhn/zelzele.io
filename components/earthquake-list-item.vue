98457960818724ec6d98243372f9df83a517fce6
Ahmet ilhan <ahmetilhan.dev@gmail.com>
2023-03-10 19:55:42 +0300

    code improved

--- components/earthquake-list-item.vue
+++ components/earthquake-list-item.vue
@@ -182,11 +182,18 @@ const share = async () => {
       const file = new File([dataBlob], "deprem.png", {
         type: "image/png",
       });
-      navigator.share({
-        title: `${data.Region.City} + ' ' + ${data.Region.District} Depremi}`,
-        text: "Son Deprem ve Tarihsel Deprem Grafiği",
-        files: [file],
-      });
+      navigator
+        .share({
+          title: `${data.Region.City} + ' ' + ${data.Region.District} Depremi}`,
+          text: "Son Deprem ve Tarihsel Deprem Grafiği",
+          files: [file],
+        })
+        .then(() => {
+          alert("is shared");
+        })
+        .catch((error) => {
+          alert("is not shared");
+        });
     } else {
       download(
         dataBlob,
